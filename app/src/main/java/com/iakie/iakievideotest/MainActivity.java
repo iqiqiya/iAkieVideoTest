705c5c40164459ee14cb25300a074e3b033ef28c
iqiqiya <43666095+iqiqiya@users.noreply.github.com>
2020-02-14 20:58:59 +0800

    页面绘制完成

--- app/src/main/java/com/iakie/iakievideotest/MainActivity.java
+++ app/src/main/java/com/iakie/iakievideotest/MainActivity.java
@@ -3,6 +3,7 @@ package com.iakie.iakievideotest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
